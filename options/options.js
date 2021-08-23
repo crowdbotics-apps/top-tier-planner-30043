85e2bf456147b44ae3878ab8eca6fb67ad8e5bd3
Crowdbotics <team@crowdbotics.com>
2021-08-23 15:19:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "top_tier_planner_30043Identifier",
+  url: "https://top_tier_planner_30043Identifier.botics.co",
+  api: "https://top_tier_planner_30043Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
